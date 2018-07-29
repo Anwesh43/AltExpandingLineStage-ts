994887997d9b38db0ff55760c5de9bf9fbad3fe1
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 14:21:05 +0530

    adding LinkedAEL class

--- AltExpandingLineStage.ts
+++ AltExpandingLineStage.ts
@@ -144,3 +144,28 @@ class AELNode {
         return this
     }
 }
+
+
+class LinkedAEL {
+
+    curr : AELNode = new AELNode(0)
+
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
