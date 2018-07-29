d2f0147dcc1d2344a246d4c454c5affc953b204b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-29 13:24:17 +0530

    adding AltExpandingLineStage class

---
+++ AltExpandingLineStage.ts
@@ -0,0 +1,36 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+class AltExpandingLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#BDBDBD'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : AltExpandingLineStage = new AltExpandingLineStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
